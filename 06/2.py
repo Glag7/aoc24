ed5f01de25d2dfc1c53c667bc11602832fe103e6
Gabriel Laguyonie <glaguyon@3E5.42angouleme.fr>
2024-12-07 22:28:16 +0100

    2 but it sucks

--- 06/2.py
+++ 06/2.py
@@ -3,7 +3,7 @@ file =  open("input", "r")
 i = 0
 j = 0
 search = 1
-s = ["a" * 1000]
+s = []
 for line in file:
     if (search):
         i += 1
@@ -11,37 +11,50 @@ for line in file:
         if (j):
             search = 0
     s.append("a" + line.rstrip().replace("^", "X") + "a")
-s.append("a" * 1000)
+s = ["a" * len(s[0])] + s + ["a" * len(s[0])]
 
+total = 0
 istart = i
 jstart = j
-total = 0
 idir = -1
 jdir = 0
-while (s[i + idir][j + jdir] != "a"): 
+
+while (s[i + idir][j + jdir] != "a"):
     new_idir = (jdir == 1) - (jdir == -1)
     new_jdir = (idir == -1) - (idir == 1)
-    while (s[i + idir][j + jdir] == "#"):
+
+    if (s[i + idir][j + jdir] == "#"):
         idir = new_idir
         jdir = new_jdir
-    new_idir = (jdir == 1) - (jdir == -1)
-    new_jdir = (idir == -1) - (idir == 1)
-    while (s[i + idir][j + jdir] == "." or s[i + idir][j + jdir] == "X"):
-        if (s[i + new_idir][j + new_jdir] == "X" and (i != istart or j != jstart)):
-            i2 = i + new_idir
-            j2 = j + new_jdir
-            while (s[i2][j2] == "X"):
-                i2 += new_idir
-                j2 += new_jdir
-            total += s[i2][j2] == "#"
+    elif (s[i + idir][j + jdir] == "." or s[i + idir][j + jdir] == "X"):
+        if (s[i + idir][j + jdir] == "."):
+            idir2 = new_idir
+            jdir2 = new_jdir
+            i2 = i + idir2
+            j2 = j + jdir2
+            epic_pos_list = [(i,j, idir, jdir)]
+            while (s[i2 + idir2][j2 + jdir2] != "a" and (i2, j2, idir2, jdir2) not in epic_pos_list):
+                while (s[i2 + idir2][j2 + jdir2] == "#"):
+                    new_idir2 = (jdir2 == 1) - (jdir2 == -1)
+                    jdir2 = (idir2 == -1) - (idir2 == 1)
+                    idir2 = new_idir2
+                while (s[i2 + idir2][j2 + jdir2] in ".X" and (i2, j2, idir2, jdir2) not in epic_pos_list):
+                    epic_pos_list.append((i2, j2, idir2, jdir2))
+                    i2 += idir2
+                    j2 += jdir2
+            #if not (i2, j2, idir2, jdir2) in epic_pos_list:
+               # for line in s:
+                #    print(line)
+                #print(epic_pos_list, i2, j2)
+            total += (i2, j2, idir2, jdir2) in epic_pos_list
+
         i += idir
         j += jdir
         stupid_list = list(s[i])
         stupid_list[j] = "X"
         s[i] = "".join(stupid_list)
 
-for line in s:
-    print(line)
+
 print("total is", total)
 
 file.close()
